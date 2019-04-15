e402a4b3853b47a490fa9620f36e6dcf0b5af4ee
Sieradzki <sierad@hotmail.fr>
2019-04-15 21:55:44 +0200

    Revert "Initial commit"
    This reverts commit 313db75fbc28ac77bd5a2e6a3eb8b8199f9b2887.

--- config/dev.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-const HOST = 'localhost';
-const DB_NAME ='blog';
-const CHARSET = 'utf8';
-const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
-const DB_USER = 'root';
-const DB_PASS = 'root';
